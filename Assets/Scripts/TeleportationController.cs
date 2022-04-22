ecca8ab487363a13d149bded39762d78f015f2d3
Darren Schack <dschack@gmail.com>
2022-04-21 19:30:51 -0700

    Added teleport reticle and linked teleportation to primary button.

---
+++ Assets/Scripts/TeleportationController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportationController : MonoBehaviour
+{
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public GameObject reticle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rightTeleportRay)
+        {
+            bool isActive = CheckIfActivated(rightTeleportRay);
+            rightTeleportRay.gameObject.SetActive(isActive);
+            reticle.SetActive(isActive);
+        }
+
+    }
+
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        bool isActivated = false;
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out isActivated);
+        return isActivated;
+    }
+}
