2fda7ae6bab5d8c1f21e0d78df316e6695afc0c0
Darren Schack <dschack@gmail.com>
2022-05-05 10:22:20 -0700

    Run a simple circuit simulation on startup to preload the simulator and avoid a long lag when connecting our first actual circuit later.

--- Assets/Scripts/CircuitLab/CircuitLab.cs
+++ Assets/Scripts/CircuitLab/CircuitLab.cs
@@ -46,6 +46,21 @@ public class CircuitLab : MonoBehaviour, ICircuitLab
 
         // Create peg objects at regular intervals across the board
         CreatePegs();
+
+        PreloadSimulator();
+    }
+
+    void PreloadSimulator()
+    {
+        // Build a simple circuit and run an analysis to preload the SpiceSharp simulator. 
+        // This avoids a multi-second lag when connecting our first circuit on the breadboard.
+        var ckt = new Circuit(
+            new VoltageSource("V1", "in", "0", 1.0),
+            new Resistor("R1", "in", "out", 1.0e4),
+            new Resistor("R2", "out", "0", 2.0e4)
+            );
+        var dc = new OP("DC 1");
+        dc.Run(ckt);
     }
 
     void Update()
